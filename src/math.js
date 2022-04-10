db1e7ff1439765267783e6400f35d296e0c07d85
taeheon <dalo952003@gmail.com>
2022-04-10 21:20:58 +0900

    first commit

---
+++ src/math.js
@@ -0,0 +1,13 @@
+// var math = math || {};
+
+// (function() {
+//     function sum(a, b){
+//         return a+b;
+//     }
+
+//     math.sum = sum;
+// })();
+
+export function sum(a, b) {
+    return a + b
+} // 전역 공간에 sum이 노출
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    publicPath: "",
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // loader는 뒤에서 부터 앞의 순서로 진행한다.
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]",
+          limit: 20000, //20kb
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync("")}
+      `,
+    }),
+  ],
+};
